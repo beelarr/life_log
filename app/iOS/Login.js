22b4de7f4b8ff86aaea43060c06b4a28db642e92
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-09-13 09:43:37 -0500

    Login page is working with state change, autocorrect off, and secure text entry

--- app/iOS/Login.js
+++ app/iOS/Login.js
@@ -3,14 +3,40 @@ import React, {Component} from 'react';
 import {
     View,
     Text,
-    AlertIOS
+    TextInput,
+    Button
 } from 'react-native';
 
 class Login extends Component {
+    state = {
+            email: '',
+            password: ''
+        };
+
+
+    submit() {
+        console.log('state of login', this.state);
+    }
     render() {
         return (
-            <View>
-                <Text>Login Page</Text>
+            <View style={{flex:1, justifyContent: 'center'}}>
+                <Text>Life Log</Text>
+                <TextInput
+                    style={{height:40}}
+                    autoCorrect = {false}
+                    placeHolder="you@gmail.com"
+                    onChangeText={email => this.setState({email: email})}
+                    value={this.state.email}/>
+                <TextInput
+                    style={{height:40}}
+                    autoCorrect = {false}
+                    placeHolder="Password"
+                    secureTextEntry={true}
+                    onChangeText={password => this.setState({password: password})}
+                    value={this.state.password}/>
+                <Button
+                    onPress={this.submit.bind(this)} //binds the component on the submit function to the state
+                    title="Login"/>
             </View>
         );
     }
