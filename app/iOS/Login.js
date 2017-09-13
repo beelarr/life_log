3e4961e12dcb9fe3c120bac825c027df6e554a8c
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-09-13 09:26:13 -0500

    Setup Navigation in index

---
+++ app/iOS/Login.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+import {
+    View,
+    Text,
+    AlertIOS
+} from 'react-native';
+
+class Login extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Login Page</Text>
+            </View>
+        );
+    }
+}
+
+export default Login;
--- index.ios.js
+++ index.ios.js
@@ -3,51 +3,28 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+import Login from './app/iOS/Login';
+
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  NavigatorIOS,
+
 } from 'react-native';
 
 export default class life_log extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        navigationBarHidden={false}
+        initialRoute={{title: "Login", component: Login}} //required for NaviOS
+        style = {{flex: 1}}
+      />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
 
 AppRegistry.registerComponent('life_log', () => life_log);
