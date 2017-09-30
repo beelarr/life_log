55e80025f4d83a4216a67314808969b7b855b22b
beelarr <bryonl@me.com>
2017-09-30 15:09:40 -0500

    Matched touch id with login look

--- app/iOS/TouchId.js
+++ app/iOS/TouchId.js
@@ -50,7 +50,7 @@ class TouchId extends Component {
 
         return ( //there cant be multiple views in the outermost node
             <ImageBackground
-                source={{uri: 'https://68.media.tumblr.com/0024999d4a54d3fa21cb4c4a81b9bc98/tumblr_ow6qt07IJr1u8wonlo1_1280.jpg'}}
+                source={{uri: 'https://78.media.tumblr.com/c652ff47f1f27525b0acc9c6df1ef8ca/tumblr_owwg5jg0zU1wb9q31o1_500.gif'}}
                 style={[styles.container]}>
                 <Icon
                     name="bookmark-o"
@@ -60,7 +60,10 @@ class TouchId extends Component {
                         textShadowColor: 'black',
                         textShadowOffset: {width: 2, height: 2},
                         textShadowRadius: 5}}/>
-                <Text style={[styles.logo, styles.customFont]}>Life Log</Text>
+                <Text style={styles.logoText}>
+                    <Text style={[styles.logoLife, styles.customFont]}>Life</Text>
+                    <Text style={[styles.logoLog, styles.customFont]}> Log</Text>
+                </Text>
             </ImageBackground>
 
         );
